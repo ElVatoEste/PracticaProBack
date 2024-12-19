f453f15790c97ac91959b9b0c0d34c22a8df9983
ElVatoEste <bigmalv@gmail.com>
2024-12-18 19:27:35 -0600

    Impl Auth, working ond validations

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsuarioModule } from '../usuario/usuario.module';
 import { JwtStrategy } from './strategie/jwt.strategy';
 
 @Module({
   imports: [
+    UsuarioModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'defaultSecret',
@@ -14,6 +16,6 @@ import { JwtStrategy } from './strategie/jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy], // Agrega JwtStrategy aquí
 })
 export class AuthModule {}
